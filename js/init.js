b3b250a039ed5fee3b2db1d5cdab15d5dae6d4d0
formrick42 <81179376+formrick42@users.noreply.github.com>
2021-03-23 07:53:55 -0600

    Create init.js

---
+++ js/init.js
@@ -0,0 +1,12 @@
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyAuO0ft5iseHBAZRDr9sDgXZYR9o6de48M",
+    authDomain: "autentica42.firebaseapp.com",
+    projectId: "autentica42",
+    storageBucket: "autentica42.appspot.com",
+    messagingSenderId: "183357676186",
+    appId: "1:183357676186:web:5ef255c8320c284c009d23",
+    measurementId: "G-S7ZSKZKK9Q"
+    });
