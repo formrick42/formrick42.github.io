8733ffa8c820d500ad513bf370ead79755f52b0e
formrick42 <81179376+formrick42@users.noreply.github.com>
2021-03-24 12:23:56 -0600

    Update init.js

--- js/init.js
+++ js/init.js
@@ -1,7 +1,12 @@
-apiKey: "AIzaSyDUX8q52b_wn-l1J5Qk4eOpJjrWku1JyEs",
+/*  Initializa Firebase con la configuración del proyecto. Revisa la
+ * configuración en tu servidor de Firebase.  */
+// @ts-ignore
+firebase.initializeApp({
+    apiKey: "AIzaSyDUX8q52b_wn-l1J5Qk4eOpJjrWku1JyEs",
     authDomain: "chat41re.firebaseapp.com",
     projectId: "chat41re",
     storageBucket: "chat41re.appspot.com",
     messagingSenderId: "897176894279",
     appId: "1:897176894279:web:58afb6d71785e192252a62",
     measurementId: "G-YFD6H0LE2S"
+    });
